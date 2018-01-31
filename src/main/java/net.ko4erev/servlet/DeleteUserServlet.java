6dc0983d83ac2cf3d88949e940788775f34b6989
Кочерев Алексей <ko4erev@yandex.ru>
2018-01-31 20:29:09 +0400

    Simple CRUD

---
+++ src/main/java/net.ko4erev/servlet/DeleteUserServlet.java
@@ -0,0 +1,43 @@
+package net.ko4erev.servlet;
+
+import net.ko4erev.model.User;
+import net.ko4erev.util.Utils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class DeleteUserServlet extends HttpServlet {
+
+    private Map<Integer, User> users;
+
+    @Override
+    public void init() throws ServletException {
+
+        final Object users = getServletContext().getAttribute("users");
+
+        if (users == null || !(users instanceof ConcurrentHashMap)) {
+
+            throw new IllegalStateException("You're repo does not initialize!");
+        } else {
+
+            this.users = (ConcurrentHashMap<Integer, User>) users;
+        }
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        req.setCharacterEncoding("UTF-8");
+
+        if (Utils.idIsNumber(req)) {
+            users.remove(Integer.valueOf(req.getParameter("id")));
+        }
+
+        resp.sendRedirect(req.getContextPath() + "/");
+    }
+}
